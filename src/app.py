1cfd03c39e349376256101f6d4bee722cb5d0681
nsaintgeours <nathalie.saint-geours@itk.fr>
2021-10-19 23:15:34 +0200

    :wrench: pass mlflow model api url to dash app

--- src/app.py
+++ src/app.py
@@ -1,7 +1,8 @@
+import os
 import dash
 
 app = dash.Dash(__name__)
-app.layout = dash.html.Div(children=[dash.html.H1('Mon projet')])
+app.layout = dash.html.Div(children=[dash.html.H1(f'Mon projet {os.getenv("MODEL_API")}')])
 
 if __name__ == '__main__':
     app.run_server(host="0.0.0.0", port=8888)
