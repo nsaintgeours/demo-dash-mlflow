806cd591dcb6037d42dec843fa64bc48173e09a4
nsaintgeours <nathalie.saint-geours@itk.fr>
2021-10-19 17:54:20 +0200

    :wrench: expose dash app on port 8888

--- src/app.py
+++ src/app.py
@@ -4,4 +4,4 @@ app = dash.Dash(__name__)
 app.layout = dash.html.Div(children=[dash.html.H1('Mon projet')])
 
 if __name__ == '__main__':
-    app.run_server(host="0.0.0.0")
+    app.run_server(host="0.0.0.0", port=8888)
